cf4c2eca40854430318322a2c3265adbbcd4348d
sphanit <phani-teja.singamaneni@inria.fr>
2026-02-25 15:21:13 +0100

    updated compile scripts

--- compile.sh
+++ compile.sh
@@ -1,15 +1,13 @@
-# Update the submodules
-git submodule update --init --recursive
+#!/bin/bash
 
 type=${1:-Release}
 
 # Build the selected packages (for development)
-if command -v catkin &> /dev/null
+if command -v colcon &> /dev/null
 then
-    catkin build -j4 -DCMAKE_BUILD_TYPE=$type -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --cmake-args -Wno-dev
+    # colcon build --symlink-install --packages-skip cohan_tutorial --cmake-args -DCMAKE_BUILD_TYPE=$type -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -Wno-dev
+    colcon build --symlink-install --packages-skip cohan_tutorial --cmake-args -DCMAKE_BUILD_TYPE=$type -Wno-dev
 else
-    echo "catkin installation not found!"
+    echo "colcon not found!"
     exit 1
 fi
-cd $PWD/build/ && jq -s add **/*.json > compile_commands.json
-echo "Combined the JSON"
--- install-deps.sh
+++ install-deps.sh
@@ -1,26 +1,5 @@
 #!/bin/bash 
-
-rosdep update --include-eol
-
-if [ -n "${ROS_DISTRO}" ];
-then
-	rosdep install --from-paths src --ignore-src --rosdistro=$ROS_DISTRO -y
-else
-	echo "ROS_DISTRO not set!"
-fi
-
-# sudo apt-get install ros-melodic-pr2-description -y \
-#                      libqt5widgets5 \
-#                      ros-melodic-mbf-costmap-core \
-#                      libqt5core5a \
-#                      ros-melodic-costmap-converter \
-#                      libzmq3-dev \
-#                      qtbase5-dev \
-#                      libsdl-image1.2-dev \
-#                      libqt5gui5 \
-#                      libsqlite3-dev \
-#                      libyaml-cpp-dev \
-#                      ros-melodic-libg2o \
-#                      ros-melodic-pr2-tuckarm \
-#                      libboost-all-dev \
-#                      ros-melodic-mbf-msgs
+sudo rosdep init
+rosdep update
+git submodule update --init --recursive
+rosdep install --from-paths src --ignore-src -r -y
