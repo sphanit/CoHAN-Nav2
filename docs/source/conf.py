fcbe9701641d8b1cbf81151b5e791e1249da7089
sphanit <phani-teja.singamaneni@inria.fr>
2026-05-27 11:36:11 +0200

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,15 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+html_static_path = ['_static']
+def setup(app):
+    app.add_css_file("custom.css")
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_show_sourcelink = False
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
